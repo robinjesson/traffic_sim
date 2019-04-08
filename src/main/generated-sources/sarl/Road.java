e0973effe46ddffd4ab5065b647dedf6718b5f8f
Robin Jesson <robin.jesson@hotmail.fr>
2019-04-08 17:40:19 +0200

    maven

---
+++ src/main/generated-sources/sarl/Road.java
@@ -0,0 +1,16 @@
+import io.sarl.lang.annotation.SarlElementType;
+import io.sarl.lang.annotation.SarlSpecification;
+import io.sarl.lang.annotation.SyntheticMember;
+
+/**
+ * @author robin
+ */
+@SarlSpecification("0.8")
+@SarlElementType(10)
+@SuppressWarnings("all")
+public class Road {
+  @SyntheticMember
+  public Road() {
+    super();
+  }
+}
