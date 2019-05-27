7bdfe57a1c141dbcca19b3b8ee64e0991923ddb2
Etienne Filhol <etienne.filhol@hotmail.fr>
2019-05-27 18:56:46 +0200

    added fix

--- src/main/generated-sources/sarl/road_elements/ShapeReader.java
+++ src/main/generated-sources/sarl/road_elements/ShapeReader.java
@@ -8,8 +8,21 @@ import io.sarl.lang.annotation.SarlSourceCode;
 import io.sarl.lang.annotation.SarlSpecification;
 import io.sarl.lang.annotation.SyntheticMember;
 import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
+import org.arakhne.afc.gis.io.shape.GISShapeFileReader;
+import org.arakhne.afc.gis.mapelement.MapElement;
 import org.arakhne.afc.gis.maplayer.MapElementLayer;
+import org.arakhne.afc.gis.maplayer.TreeMapElementLayer;
+import org.arakhne.afc.gis.road.RoadPolyline;
+import org.arakhne.afc.gis.road.StandardRoadNetwork;
 import org.arakhne.afc.gis.road.layer.RoadNetworkLayer;
+import org.arakhne.afc.gis.road.primitive.RoadSegment;
+import org.arakhne.afc.io.dbase.DBaseFileFilter;
+import org.arakhne.afc.io.shape.ESRIBounds;
+import org.arakhne.afc.math.geometry.d2.d.Rectangle2d;
+import org.arakhne.afc.vmutil.FileSystem;
+import org.eclipse.xtext.xbase.lib.Exceptions;
 
 @SarlSpecification("0.9")
 @SarlElementType(10)
@@ -38,19 +51,77 @@ public class ShapeReader {
    */
   @DefaultValueSource
   public MapElementLayer<?> loadShapeFile(final File file, @DefaultValue("road_elements.ShapeReader#LOADSHAPEFILE_0") final boolean isRoad) {
-    throw new Error("Unresolved compilation problems:"
-      + "\nDBaseFileFilter cannot be resolved to a type."
-      + "\nGISShapeFileReader cannot be resolved."
-      + "\nEXTENSION_DBASE_FILE cannot be resolved"
-      + "\ngetBoundsFromHeader cannot be resolved"
-      + "\nminX cannot be resolved"
-      + "\nminY cannot be resolved"
-      + "\nmaxX cannot be resolved"
-      + "\nmaxY cannot be resolved"
-      + "\nmapElementType cannot be resolved"
-      + "\nread cannot be resolved"
-      + "\nread cannot be resolved"
-      + "\nclose cannot be resolved");
+    try {
+      StandardRoadNetwork network = null;
+      MapElementLayer<MapElement> layer = null;
+      File dbfFile = FileSystem.replaceExtension(file, DBaseFileFilter.EXTENSION_DBASE_FILE);
+      URL _xifexpression = null;
+      boolean _canRead = dbfFile.canRead();
+      if (_canRead) {
+        _xifexpression = ((dbfFile == null ? null : dbfFile.toURI()) == null ? null : (dbfFile == null ? null : dbfFile.toURI()).toURL());
+      } else {
+        _xifexpression = null;
+      }
+      URL dbfUrl = _xifexpression;
+      FileInputStream is = new FileInputStream(file);
+      try {
+        class $AssertEvaluator$ {
+          final boolean $$result;
+          $AssertEvaluator$(final FileInputStream is) {
+            this.$$result = (is != null);
+          }
+        }
+        assert new $AssertEvaluator$(is).$$result;
+        GISShapeFileReader reader = new GISShapeFileReader(is, null, dbfUrl);
+        try {
+          final Rectangle2d worldRect = new Rectangle2d();
+          final ESRIBounds esriBounds = reader.getBoundsFromHeader();
+          worldRect.setFromCorners(esriBounds.getMinX(), esriBounds.getMinY(), esriBounds.getMaxX(), esriBounds.getMaxY());
+          if (isRoad) {
+            reader.setMapElementType(RoadPolyline.class);
+          }
+          MapElement element = null;
+          if (isRoad) {
+            while (((element = reader.read()) != null)) {
+              if ((element instanceof RoadPolyline)) {
+                if ((network == null)) {
+                  StandardRoadNetwork _standardRoadNetwork = new StandardRoadNetwork(worldRect);
+                  network = _standardRoadNetwork;
+                }
+                network.addRoadSegment(((RoadSegment)element));
+              } else {
+                if ((layer == null)) {
+                  TreeMapElementLayer<MapElement> _treeMapElementLayer = new TreeMapElementLayer<MapElement>(worldRect);
+                  layer = _treeMapElementLayer;
+                }
+                layer.addMapElement(element);
+              }
+            }
+          } else {
+            while (((element = reader.read()) != null)) {
+              {
+                if ((layer == null)) {
+                  TreeMapElementLayer<MapElement> _treeMapElementLayer = new TreeMapElementLayer<MapElement>(worldRect);
+                  layer = _treeMapElementLayer;
+                }
+                layer.addMapElement(element);
+              }
+            }
+          }
+        } finally {
+          reader.close();
+        }
+      } finally {
+        is.close();
+      }
+      if ((network != null)) {
+        final RoadNetworkLayer networkLayer = new RoadNetworkLayer(network);
+        return networkLayer;
+      }
+      return layer;
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
   }
   
   /**
--- src/main/generated-sources/sarl/road_elements/Test.java
+++ src/main/generated-sources/sarl/road_elements/Test.java
@@ -5,8 +5,14 @@ import io.sarl.lang.annotation.SarlSpecification;
 import io.sarl.lang.annotation.SyntheticMember;
 import java.util.ArrayList;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import org.arakhne.afc.gis.maplayer.MultiMapLayer;
+import org.arakhne.afc.gis.ui.GisCanvas;
+import org.arakhne.afc.nodefx.ZoomablePane;
 import org.eclipse.xtext.xbase.lib.Pure;
 import road_elements.Road;
 import road_elements.RoadObject;
@@ -30,9 +36,19 @@ public class Test extends Application {
   
   @Override
   public void start(final Stage primaryStage) throws Exception {
-    throw new Error("Unresolved compilation problems:"
-      + "\nZoomablePane cannot be resolved."
-      + "\nGisCanvas cannot be resolved.");
+    MultiMapLayer loadedResource = this.getRoads();
+    BorderPane root = new BorderPane();
+    Label messageBar = new Label("");
+    messageBar.setTextAlignment(TextAlignment.CENTER);
+    MultiMapLayer<MultiMapLayer> rootLayer = new MultiMapLayer<MultiMapLayer>();
+    rootLayer.addMapLayer(loadedResource);
+    GisCanvas<MultiMapLayer<MultiMapLayer>> _gisCanvas = new GisCanvas<MultiMapLayer<MultiMapLayer>>(rootLayer);
+    ZoomablePane<MultiMapLayer<MultiMapLayer>> scrollPane = new ZoomablePane<MultiMapLayer<MultiMapLayer>>(_gisCanvas);
+    root.setCenter(scrollPane);
+    root.setBottom(messageBar);
+    Scene scene = new Scene(root, 1024, 768);
+    primaryStage.setScene(scene);
+    primaryStage.show();
   }
   
   @SyntheticMember
